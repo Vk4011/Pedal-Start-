683ae3d708bdb44b1a13802f14292977a53b15ea
Vk4011 <gvikramram63@gmail.com>
2024-06-27 11:12:43 +0530

    feat : api  🔥

--- frontend/src/components/Base_Url.jsx
+++ frontend/src/components/Base_Url.jsx
@@ -1,5 +1,6 @@
 
 
-const API_URL = 'http://localhost:5000/api/tasks';
+// const API_URL = 'http://localhost:5000/api/tasks';
+const API_URL = 'https://pedal-start-kqlm.onrender.com/api/tasks';
 
 export default API_URL;
